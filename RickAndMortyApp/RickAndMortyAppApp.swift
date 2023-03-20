a21de5558cb6ce7b213277e86f5db219a21bab6c
Francisco Javier Alarza <fjalarza@viewnext.com>
2023-03-20 17:00:09 +0100

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Francisco Javier Alarza Sanchez on 20/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
